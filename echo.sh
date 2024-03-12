bd813057fb4529c002292e154eff7bb26e3cf0b5
PriyanshuRajvishal103 <priyanshu01032003@gmail.com>
2024-03-12 21:15:20 +0530

    shellscripting

---
+++ echo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+function add(){
+        local num1=$1
+        local num2=$2
+        local sum=$((num1+num2))
+        echo "the sum of  $num1 and $num2 is $sum"
+}
+
+if [ $# -eq 2 ] ;
+then
+        add $1 $2
+else
+        echo "wrong parameter"
